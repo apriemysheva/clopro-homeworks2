e430a1520daaed7e4a931ea48f70076e1e3c1069
Anna <apriemysheva@whoosh.bike>
2023-11-11 22:39:15 +0700

    1112

---
+++ terraform/variables.tf
@@ -0,0 +1,39 @@
+variable "yandex_cloud_id" {
+  default = "b1gagslfe3eas6a9kdt1"
+}
+
+variable "yandex_folder_id" {
+  default = "b1gni5ssvntg578r3btp"
+}
+
+variable "a-zone" {
+  default = "ru-central1-c"
+}
+
+variable "nat-instance-image-id" {
+  default = "fd80mrhj8fl2oe87o4e1"
+}
+
+variable "nat-instance-ip" {
+  default = "192.168.10.254"
+}
+
+variable "centos-7-base" {
+  default = "fd80g1svihsdmjv1p4k8"
+}
+
+variable "domain" {
+  default = "netology.cloud"
+}
+
+variable "nat-instance-name" {
+  default = "nat-instance-vm1"
+}
+
+variable "public-vm-name" {
+  default = "public-vm1"
+}
+
+variable "private-vm-name" {
+  default = "private-vm1"
+}
